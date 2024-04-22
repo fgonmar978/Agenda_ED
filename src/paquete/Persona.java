4259e5839a48cc028d226b01778dc5dadc485327
sweiji0409 <163828682+sweiji0409@users.noreply.github.com>
2024-04-22 10:22:39 +0200

    Commit inicial
    Creación del proyecto

---
+++ src/paquete/Persona.java
@@ -0,0 +1,19 @@
+package paquete;
+
+public class Persona {
+
+	/**
+	 * Atributo: nombre
+	 */
+	private String nombre;
+	
+	/**
+	 * Atributo apellido
+	 */
+	private String apellidos;
+	
+	/**
+	 * Atributo fecha de nacimiento
+	 */
+	private int fechanac;
+}
