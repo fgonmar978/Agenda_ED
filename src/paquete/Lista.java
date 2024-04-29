e9f233ad6712652f8be0b9edebccc1821b9fd153
sweiji0409 <163828682+sweiji0409@users.noreply.github.com>
2024-04-29 10:15:09 +0200

    Update Lista.java
    Creación del atributo, constructor para el array y el métod addContact.

--- src/paquete/Lista.java
+++ src/paquete/Lista.java
@@ -3,4 +3,32 @@ package paquete;
 public class Lista
 {
 
+	/**
+	 * Atributo : array de contacto
+	 */
+	private Contacto contactos[];
+	
+	/**
+	 * Constructor: crea el array contactos
+	 */
+	public Lista() {
+		contactos = new Contacto[10];
+	}
+	
+	/**
+	 * Descripción: añade un contacto nuevo en la lista
+	 */
+	public boolean addContact(Contacto c) {
+		
+		int i = 0;
+		for(i = 0; i < contactos.length; i++) {
+			if(contactos[i] == null) {
+				contactos[i] = c;
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
 }
