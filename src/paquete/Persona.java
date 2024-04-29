477611edec146a1b54efaa8cad810d0b66bfde5c
lmiggon294 <163828699+lmiggon294@users.noreply.github.com>
2024-04-29 10:10:33 +0200

    Persona clase base

--- src/paquete/Persona.java
+++ src/paquete/Persona.java
@@ -1,19 +1,105 @@
+
+/**
+ * <h2>Clase persona</h2>
+ * Representa una persona
+ * @version 0.01
+ * @author lmiggon294@g.educaand.es
+ * @since 04/2024
+ */
+
 package paquete;
+
+import java.time.LocalDate;
 
 public class Persona {
 
 	/**
 	 * Atributo: nombre
 	 */
-	private String nombre;
+	protected String nombre;
 	
 	/**
 	 * Atributo apellido
 	 */
-	private String apellidos;
+	protected String apellidos;
 	
 	/**
 	 * Atributo fecha de nacimiento
 	 */
-	private int fechanac;
+	protected LocalDate fechaNac;
+	
+	/**
+	 * Constructor único
+	 * <b>Descripcion</b>:Crea el objeto persona con nombre, apellidos y fecha de nacimiento
+	 * @param nombre <i> Nombre de la persona </i>
+	 * @param apellidos <i> Apellido de la persona </i>
+	 * @param fechaNac <i> Fecha de nacimiento de la persona<i>
+	 * */
+	
+	public Persona(String nombre, String apellidos, LocalDate fechaNac) {
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.fechaNac = fechaNac;
+	}
+	
+	/**
+	 *<b>Descripcion</b>:Retorna el nombre de la persona
+	 *<b>Parámetros</b>
+	 *<b>Valor retornado</b>:Cadena de caracteres que representa el nombre
+	 *<b>Estado</b>: Funcional
+	 * */
+	public String getNombre() {
+		return nombre;
+	}
+	/**
+	 *<b>Descripcion</b>:Cambia el nombre de la persona
+	 *<b>Parámetros</b>:Cadena de caracteres
+	 *<b>Valor retornado</b>
+	 *<b>Estado</b>: Funcional
+	 * */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	/**
+	 *<b>Descripcion</b>:Retorna los apellidos de la persona
+	 *<b>Parámetros</b>
+	 *<b>Valor retornado</b>:Cadena de caracteres que representa los apellidos
+	 *<b>Estado</b>: Funcional
+	 * */
+	public String getApellidos() {
+		return apellidos;
+	}
+	/**
+	 *<b>Descripcion</b>:Cambia los apellidos de la persona
+	 *<b>Parámetros</b>:Cadena de caracteres
+	 *<b>Valor retornado</b>
+	 *<b>Estado</b>: Funcional
+	 * */
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	
+	/**
+	 *<b>Descripcion</b>:Retorna la fecha de nacimiento de la persona
+	 *<b>Parámetros</b>
+	 *<b>Valor retornado</b>:Fecha que representa la fecha de nacimiento
+	 *<b>Estado</b>: Funcional
+	 * */
+	public LocalDate getFechaNac() {
+		return fechaNac;
+	}
+	/**
+	 *<b>Descripcion</b>:Cambia el nombre de la persona
+	 *<b>Parámetros</b>:LocalDate
+	 *<b>Valor retornado</b>
+	 *<b>Estado</b>: Funcional
+	 * */
+	public void setFechaNac(LocalDate fechaNac) {
+		this.fechaNac = fechaNac;
+	}
+	
+	
+	
+	
 }
