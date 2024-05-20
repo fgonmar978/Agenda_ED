8932b7c2998239a5038f49d35377fdbf096d360f
sweiji0409 <163828682+sweiji0409@users.noreply.github.com>
2024-05-20 10:19:40 +0200

    JUnit
    Creación de PersonaTest.java

---
+++ src/pruebas/PersonaTest.java
@@ -0,0 +1,44 @@
+package pruebas;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PersonaTest {
+
+	@Test
+	void testPersona() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetNombre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetNombre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetApellidos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetApellidos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetFechaNac() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetFechaNac() {
+		fail("Not yet implemented");
+	}
+
+}
